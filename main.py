9c5f3f74ad490e4843930b08fadf99886d25e800
owls4all <owls4all@gmail.com>
2025-03-24 12:14:00 -0700

    changed plan for accuracy-based # of questions

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
 # Write your code here
 from utility import *
+class quest:
+    def __init__(self,string):
+        self.data = str_to_dict(string,"|||","<<>>")
+        for key in self.data.keys():
+            if key in ["times wrong","times asked","times to show"]:
+                self.data[key] = int(self.data[key])
+            elif key == 'accuracy':
+                self.data[key] = float(self.data[key])
+    def stringify(self):
+        return dict_to_str(self.data,"|||","<<>>")
+    def oneLessQuestion(self):
+        if self.data["times to show"] >=2:
+            self.data["times to show"] -=1
+    def askUser(self):
+        response = ask(self.data["question"])
+        self.data["times asked"] +=1
+        if response != self.data["answer"]:
+            self.data["times wrong"] +=1
+            self.data["times to show"] +=1
+        asked = self.data["times asked"]
+        wrong = self.data["times wrong"]
+        self.data["accuracy"] = (asked - wrong)/asked
+        if self.data["accuracy"] > 0.75:            
+            self.oneLessQuestion()
+        if self.data["accuracy"] > 0.9:
+            self.oneLessQuestion()
+    def showStats(self):
+        statsToShow = f'''{"="*24}
+{self.data["question"]}
+{self.data["answer"]}
+
+Times asked: {self.data["times asked"]}
+Times correct: {self.data["times asked"]-self.data["times wrong"]}
+Times incorrect: {self.data["times wrong"]}
+Accuracy: {self.data["accuracy"]}
+{"="*24}
+'''
--- proposal.md
+++ proposal.md
@@ -14,7 +14,8 @@ A3: 100
 
 3. How will the number of times you show each flashcard change the second time the user runs the program, based on their performance the previous time? 
     +1 for every time it is answered wrong
-    -1 for every perfect set    
+    -1 if accuracy > 3/4
+    -1 if accuracy > 9/10
 
 4. What information will you need to store in a file in order to make the program change it's behavior each time the user runs the program?
 user name
