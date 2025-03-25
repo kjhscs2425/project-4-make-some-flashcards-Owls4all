83f477402da7a090e3bd5b70e5b56eb79cb2e341
owls4all <owls4all@gmail.com>
2025-03-25 09:04:13 -0700

    shortened keys for convenience

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 # Write your code here
+import os
 from utility import *
 questionList = []
 class quest:
     def __init__(self,dataDict:dict):
         self.data = dataDict
         print(self.data)
-        for key in ["times wrong","times asked","times to show","accuracy"]:
-            if key in ["times wrong","times asked","times to show"]:
+        for key in self.data.keys():
+            if key in ["wrong","asked","show"]:
                 self.data[key] = int(self.data[key])
             elif key == 'accuracy':
                 self.data[key] = float(self.data[key])
     def stringify(self):
         return dict_to_str(self.data,"|||","<<>>")
     def oneLessQuestion(self):
-        if self.data["times to show"] >=2:
-            self.data["times to show"] -=1
+        if self.data["show"] >=2:
+            self.data["show"] -=1
     def askUser(self):
         response = ask(self.data["question"])
-        self.data["times asked"] +=1
+        self.data["asked"] +=1
         if response != self.data["answer"]:
-            self.data["times wrong"] +=1
-            self.data["times to show"] +=1
-        asked = self.data["times asked"]
-        wrong = self.data["times wrong"]
+            self.data["wrong"] +=1
+            self.data["show"] +=1
+        asked = self.data["asked"]
+        wrong = self.data["wrong"]
         self.data["accuracy"] = (asked - wrong)/asked
         if self.data["accuracy"] > 0.75:            
             self.oneLessQuestion()
@@ -33,16 +34,16 @@ class quest:
 {self.data["question"]}
 {self.data["answer"]}
 
-Times asked: {self.data["times asked"]}
-Times correct: {self.data["times asked"]-self.data["times wrong"]}
-Times incorrect: {self.data["times wrong"]}
+Times asked: {self.data["asked"]}
+Times correct: {self.data["asked"]-self.data["wrong"]}
+Times incorrect: {self.data["wrong"]}
 Accuracy: {self.data["accuracy"]}
 {"="*24}
 '''
         print(statsToShow)
 user = ask("User name?")
 #'''
-if False: #[check for user file]
+if os.path.isfile("data.json"):
     source_file = open(f"{user}.txt","r").read()
 else:
     source_file = open("default.txt","r").read()
@@ -52,7 +53,7 @@ else:
         questionList.append(newQuest)
 cards = []
 for q in questionList:
-    foo = q.data["times to show"]
+    foo = q.data["show"]
     for i in range(foo):
         cards.append(indexInList(q,questionList))
 
@@ -74,7 +75,7 @@ addingQuestions = True
 my_data_string = ''
 while addingQuestions:
     data = {}
-    for key in ["question","answer","accuracy","times to show","times asked","times wrong"]:
+    for key in ["question","answer","accuracy","show","asked","wrong"]:
         data[key] = ask(key)
     stringVersion = (dict_to_str(data,"|||","<<>>"))
     my_data_string += f"{stringVersion}###\n"
