9aff2dbd2f85c45b09af796df359760de79a96ab
owls4all <owls4all@gmail.com>
2025-04-09 12:36:53 -0700

    Adding comments (about 1/3 of the way through)

--- main.py
+++ main.py
@@ -1,75 +1,88 @@
 # Write your code here
-import os
-import time
-from utility import *
-questionList = []
-source_file = ''
-thisRunData = {
+import os #for checking if files exist
+import time #for timing runs
+from utility import * #primarily for 'ask', but also for 'YesList' and some other stuff 
+questionList = [] #make an empty list - we append questions as we read from the file
+source_file = '' 
+thisRunData = { #dictionary of current stats for the run
     "asked":0,
-    "wrong":0,
+    "wrong":0, #right can be calculated 'asked - wrong' and thus need not be tracked separately
     "accuracy":'---',
-    "order":''
+    "order":'' #show history of right/wrong answers chronologically with '√' or 'x'
 }
-boxes = [100,95,90,85,80]
-class quest:
-    def __init__(self,dataDict:dict):
+boxes = [100,95,90,85,80] # % chance to show question (per instance)
+class quest: 
+    def __init__(self,dataDict:dict):  #establish a question with a dictionary of stats.
+        #the stats, in order, are:
+        '''
+        question - Text prompt shown to the user.
+        answer - expected response from the user.
+        units - Optional 2nd part of response, such as specifying 'stone' or 'g' when asked how much something costs.
+        accuracy - what fraction of this question's answers have been correct
+        show - how many times to show the question. Does not account for high box skipping questions. 
+            (default 2. Perfect runs decrease it by 1. mistakes increase it by 1)
+        asked - the number of times the question has ever been asked
+        wrong - the number of times the question has been gotten wrong
+        order - the question's lifetime history of right/wrong answers, represented with 'x' or '√' accordingly
+        box - leitner system box. Higher-boxed questions have a 5*box % chance to not be shown
+        '''
         self.data = dataDict
-        self.ask = 0
-        self.wrong = 0
-        self.accuracy = '---'
-        if type(self.data)==dict:
-            for key in self.data.keys():
-                if key in ["wrong","asked","show",'box']:
-                    self.data[key] = int(self.data[key])
-                elif key == 'accuracy':
-                    self.data[key] = float(self.data[key])
-            if 'box' not in self.data.keys():
-                self.data["box"] = 0
+        self.ask = 0 #single-run stat
+        self.wrong = 0 #single-run stat
+        self.accuracy = '---' #single-run stat
+        if type(self.data)==dict: #in past versions it wasn't. this is to prevent TypeErrors
+            for key in self.data.keys(): #the way the dictionary is read from the file, all stats start as strings.
+                if key in ["wrong","asked","show",'box']: #checking stats one at a time
+                    self.data[key] = int(self.data[key]) #those which should be integers are converted here
+                elif key == 'accuracy': 
+                    self.data[key] = float(self.data[key])#that which should be a float is converted here
+            if 'box' not in self.data.keys():#Some of the older save files did not have a 'box' stat.
+                self.data["box"] = 0 #This adds the stat with the default value in that case
         else:
-            print(f"self.data is a {type(self.data)}")
-    def stringify(self):
-        return dict_to_str(self.data,"|||","<<>>")
-    def oneLessQuestion(self):
+            print(f"self.data is a {type(self.data)}") #error message printed rather than causing a crash
+    def stringify(self): #data is stored in a file as a string with dividers between key-value pairs
+        return dict_to_str(self.data,"|||","<<>>") #converts the data to a string, with '<<>>' between a key and its value, and '|||' between pairs, so it can be interpreted again later.
+    def oneLessQuestion(self): #lowers show stat, but not less than 1.
         if self.data["show"] >=2:
             self.data["show"] -=1
-    def askUser(self):
-        if type(self.data) != dict:
+    def askUser(self): 
+        if type(self.data) != dict: #primarily to prevent errors which have been patched out. Kept primarily out of laziness, but also the remote chance that a similar error be introduced again later.
             print(f"self.data is a {type(self.data)}")
             return
-        if "question" not in self.data.keys():
+        if "question" not in self.data.keys(): #skip questions with no actual question
             return
-        response = ask(self.data["question"]).lower()
-        self.data["asked"] +=1
+        response = ask(self.data["question"]).lower() #get user input
+        self.data["asked"] +=1 #increment stats as is appropriate
         self.ask +=1
         thisRunData["asked"]+=1
-        correctOptions=[self.data["answer"],self.data["answer"]+self.data["units"]]
-        if response not in correctOptions:
-            self.box = 0
-            self.data["wrong"] +=1
+        correctOptions=[self.data["answer"],self.data["answer"]+self.data["units"]] #acceptable answers
+        if response not in correctOptions: #if the user is wrong...
+            self.box = 0 #put the question back into the first box. (no skipping it)
+            self.data["wrong"] +=1 #increment stats as is appropriate
             self.wrong +=1
             thisRunData["wrong"]+=1
             self.data["show"] +=1
-            print(f"Incorrect! The answer is {self.data["answer"]}{self.data["units"]}.")
-            self.data["order"]+='x '
-            thisRunData["order"]+='x '
-        else:
-            print("Correct!")
-            self.data["order"]+='√ '
-            thisRunData["order"]+='√ '
-        asked = self.data["asked"]
-        thisRunAsked = thisRunData["asked"]
-        wrong = self.data["wrong"]
-        thisRunWrong = thisRunData["wrong"]
-        self.data["accuracy"] = (asked - wrong)/asked
-        self.accuracy = (self.ask-self.wrong)/self.ask
-        thisRunData["accuracy"] = (thisRunAsked-thisRunWrong)/(thisRunAsked)
+            print(f"Incorrect! The answer is {self.data["answer"]}{self.data["units"]}.")#tell the user they got it wrong. (and what they should have said!)
+            self.data["order"]+='x ' #make a note in the question history of the wrong answer
+            thisRunData["order"]+='x '#and in the run history
+        else: #if they didn't get it wrong...
+            print("Correct!") #tell them they got it right!
+            self.data["order"]+='√ '#record it in the question's history
+            thisRunData["order"]+='√ '#and in the run history
+        asked = self.data["asked"]#shortcut
+        thisRunAsked = thisRunData["asked"]#shortcut
+        wrong = self.data["wrong"]#shortcut
+        thisRunWrong = thisRunData["wrong"]#shortcut
+        self.data["accuracy"] = (asked - wrong)/asked#recalculate question's accuracy
+        self.accuracy = (self.ask-self.wrong)/self.ask#calculate run-specific question accuracy
+        thisRunData["accuracy"] = (thisRunAsked-thisRunWrong)/(thisRunAsked)#recalculate overall run accuracy
     def showStats(self):
         if type(self.data) != dict:
             print(type(self.data))
             return
         for stat in ["question"]:
             if stat not in self.data.keys():
-                #print(f"No {stat} stat!")
+                #print(f"No {stat} stat!") #former debugging statements
                 #print(dict_to_str(self.data,"|||","<<>>"))
                 global questionList
                 questionList.__delitem__(indexInList(self,questionList))
